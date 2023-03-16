d5cbbc0bbd45b18932526f314600ee5c49298613
keyssonG <104814645+KeyssonG@users.noreply.github.com>
2023-03-16 01:35:07 -0300

    atualizacoes

---
+++ aula18.py
@@ -0,0 +1,22 @@
+# if / elif      / else
+# se / se não se / se não
+condicao1 = False
+condicao2 = False
+condicao3 = False
+condicao4 = False
+
+if condicao1:
+    print('Este para condição 1')
+elif condicao2:
+    print('Este para condição 2')
+elif condicao3:
+    print('Este para condição 3')
+elif condicao4:
+    print('Este para condição 4')
+else:
+    print('Nenhuma condição foi satisfeita.')
+
+if 10 == 10:
+    print('Outro if')
+
+print('Fora do if')
