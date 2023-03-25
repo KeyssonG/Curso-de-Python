514d805835cf6b02766fea7ac53ad9fe417c9995
keyssonG <104814645+KeyssonG@users.noreply.github.com>
2023-03-25 11:05:35 -0300

    fatiamento_de_strings

---
+++ aula27.py
@@ -0,0 +1,12 @@
+"""
+Fatiamente de strints
+012345678
+Olá mundo
+-987654321
+Fatiamente [i:f:p] [::]
+Obs.: a função len retorna a qtd
+de caracteres da str
+"""
+
+variavel = 'Olá mundo'
+print(variavel[::-1])
