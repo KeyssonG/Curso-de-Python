0e65106db4470b89254ba1861a8135349094a05a
keyssonG <104814645+KeyssonG@users.noreply.github.com>
2023-04-15 22:42:14 -0300

    while

---
+++ aula39.py
@@ -0,0 +1,13 @@
+"""
+Iterando strings com while
+"""
+#           012345678910  # Iteráveis
+nome =     'Luiz Otávio'
+#          1110987654321
+tamanho_nome = len(nome)
+print(nome)
+print(tamanho_nome)
+print(nome[3]) 
+
+nova_string += '*L*u*i*z* *O*t*á*v*i*o'
+
