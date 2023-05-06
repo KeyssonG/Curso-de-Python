d988dc1d762ddd0782f8af2203a65138c6701ae5
keyssonG <104814645+KeyssonG@users.noreply.github.com>
2023-05-06 12:59:15 -0300

    range_\_for

---
+++ aula44.py
@@ -0,0 +1,13 @@
+"""
+For + Range
+range -> range(start, stop, step)
+"""
+
+numeros = range(0, 100, 8)
+
+for numero in numeros:
+    print(numero)
+
+"""
+Na função For, a variavel numero (interador) puxa individualmente os valores da variavel numeros, pois o range agrupa o valor inteiro, enquanto a função for, puxa cada um dos valores de acordo com a ordem inserida no range.)
+"""
