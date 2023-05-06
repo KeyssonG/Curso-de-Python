c3dfee543ca52aec61285df2155a1b874d19d070
keyssonG <104814645+KeyssonG@users.noreply.github.com>
2023-05-06 17:40:45 -0300

    iterador

---
+++ aula45.py
@@ -0,0 +1,20 @@
+"""
+Iterador -> quem sabe entregar um valor por vez
+next -> me entregue o próximo valor
+iter -> me entregue seu iterador
+"""
+
+# for letra in texto
+texto = 'Keysson' #iterável
+
+# iteratador = iter(texto) # iterator
+
+# while True:
+#   try:
+#       letra = next(iteratador)
+#       print(letra)
+#   except StopIteration:
+#       break
+
+for letra in texto:
+    print(letra)
